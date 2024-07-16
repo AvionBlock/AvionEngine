66c7b0530954ae8af3fa0f5f170076823cde2c7e
SineVector241 <69619913+SineVector241@users.noreply.github.com>
2024-07-16 13:25:55 +1000

    Add stuff.

---
+++ AvionEngine/Interfaces/IRenderer.cs
@@ -0,0 +1,8 @@
+﻿namespace AvionEngine.Interfaces
+{
+    public interface IRenderer
+    {
+        IShader CreateShader(string vertexCode, string fragmentCode);
+        void AddShader(IShader shader);
+    }
+}
